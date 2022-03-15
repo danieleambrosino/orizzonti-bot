cc2b9bc5dad5cb0d33a4f9560582c8040e8de88a
Daniele Ambrosino <mail@danieleambrosino.it>
2022-03-15 21:07:03 +0100

    perf: handle filesystem access correctly
    - avoid realpath to bypass caching problems
    - ensure `/var` directory exists

--- src/Dao/FileDao.php
+++ src/Dao/FileDao.php
@@ -8,20 +8,24 @@ class FileDao implements DaoInterface
 {
 	const ROOT = __DIR__ . '/../../var';
 
+	public function __construct()
+	{
+		if (!file_exists(self::ROOT) && !mkdir(self::ROOT)) {
+			throw new \Exception('Cannot create persistence directory!');
+		}
+	}
+
 	public function find(int $id): ?Presentation
 	{
-		$path = realpath(FileDao::ROOT . '/' . $id);
-		if ($path === false) return null;
-		return unserialize(file_get_contents($path));
+		$filename = join(DIRECTORY_SEPARATOR, [self::ROOT, $id]);
+		if (!file_exists($filename)) return null;
+		return unserialize(file_get_contents($filename));
 	}
 
 	public function persist(Presentation $presentation)
 	{
-		$path = realpath(FileDao::ROOT . '/' . $presentation->getUserId());
-		if ($path === false) {
-			touch(FileDao::ROOT . '/' . $presentation->getUserId());
-			$path = realpath(FileDao::ROOT . '/' . $presentation->getUserId());
-		}
-		file_put_contents($path, serialize($presentation));
+		$filename = join(DIRECTORY_SEPARATOR, [self::ROOT, $presentation->getUserId()]);
+		if (!file_exists($filename)) touch($filename);
+		file_put_contents($filename, serialize($presentation));
 	}
 }
